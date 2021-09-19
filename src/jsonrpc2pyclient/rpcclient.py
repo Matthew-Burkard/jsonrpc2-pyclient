31c0a82117598043eae6db46f81d8d6c20c229fd
Matthew Burkard <matthewburkard@protonmail.com>
2021-09-19 11:46:52 -0400

    feat: manage ids systematically
    Client manages an internal map of ids.
    Each new id will be the current max +1.
    When a response is received that id is removed from the map.

--- src/jsonrpc2pyclient/rpcclient.py
+++ src/jsonrpc2pyclient/rpcclient.py
@@ -2,7 +2,6 @@ import abc
 import json
 import logging
 from json import JSONDecodeError
-from random import randint
 from typing import Any, Union, Type, Optional
 
 from jsonrpcobjects.errors import (
@@ -26,13 +25,17 @@ log = logging.getLogger(__name__)
 
 class RPCClient(abc.ABC):
 
+    def __init__(self) -> None:
+        self._ids = {}
+
     @property
     def server_errors(self) -> dict[int, Type]:
         return {}
 
-    @staticmethod
-    def _gen_id() -> Union[str, int]:
-        return randint(1, sys.maxsize)
+    def _get_id(self) -> int:
+        new_id = (max(self._ids.values()) or 0) + 1
+        self._ids[new_id] = new_id
+        return new_id
 
     @abc.abstractmethod
     def _send_and_get_json(self, request_json: str) -> Union[bytes, str]: ...
@@ -45,17 +48,22 @@ class RPCClient(abc.ABC):
         # Build request object.
         if params is not None:
             request = RequestObjectParams(
-                id=self._gen_id(),
+                id=self._get_id(),
                 method=method,
                 params=params,
             )
         else:
-            request = RequestObject(id=self._gen_id(), method=method)
+            request = RequestObject(id=self._get_id(), method=method)
         # Send request JSON and get JSON response.
         data = self._send_and_get_json(request.json())
         # Return result or raise error.
         try:
             json_data = json.loads(data)
+            if resp_id := json_data.get('id'):
+                try:
+                    self._ids.pop(resp_id)
+                except KeyError:
+                    pass
             if json_data.get('error'):
                 resp = ErrorResponseObject(**json_data)
                 if json_data['error'].get('data'):
