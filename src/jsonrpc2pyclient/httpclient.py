5aef260a9989c1f7c0c972af347745f65eb14ca1
Matthew Burkard <matthewburkard@protonmail.com>
2021-09-27 20:07:05 -0400

    fix: httpclient init issues
    Super RPCClient init.
    Fix naming convention on content type header.

--- src/jsonrpc2pyclient/httpclient.py
+++ src/jsonrpc2pyclient/httpclient.py
@@ -9,9 +9,10 @@ class RPCHTTPClient(RPCClient):
     def __init__(self, url: str, headers: Optional[dict] = None) -> None:
         self.session = Session()
         headers = headers or {}
-        headers['contentType'] = 'application/json'
+        headers['Content-Type'] = 'application/json'
         self.session.headers = headers
         self.url = url
+        super(RPCHTTPClient, self).__init__()
 
     def _send_and_get_json(self, request_json: str) -> Union[bytes, str]:
         return self.session.post(url=self.url, data=request_json).content
