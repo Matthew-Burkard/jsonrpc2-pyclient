ad96e850f21daa01ba6def1a56b07a4bc3937b00
Matthew Burkard <matthewjburkard@gmail.com>
2021-12-17 20:47:12 -0500

    feat!: make httpx client private
    Make httpx client private and add headers property in http rpc client.
    The "headers" property is a wrapper for the httpx client headers.

--- jsonrpc2pyclient/httpclient.py
+++ jsonrpc2pyclient/httpclient.py
@@ -3,6 +3,7 @@ import asyncio
 from typing import Optional, Union
 
 import httpx
+from httpx import Headers
 
 from jsonrpc2pyclient.rpcclient import AsyncRPCClient, RPCClient
 
@@ -13,33 +14,51 @@ class AsyncRPCHTTPClient(AsyncRPCClient):
     """A JSON-RPC HTTP Client."""
 
     def __init__(self, url: str, headers: Optional[dict] = None) -> None:
-        self.client = httpx.AsyncClient()
+        self._client = httpx.AsyncClient()
         headers = headers or {}
         headers["Content-Type"] = "application/json"
-        self.client.headers = headers
+        self._client.headers = headers
         self.url = url
         super(AsyncRPCHTTPClient, self).__init__()
 
     def __del__(self) -> None:
-        asyncio.ensure_future(self.client.aclose())
+        asyncio.ensure_future(self._client.aclose())
+
+    @property
+    def headers(self) -> Headers:
+        """HTTP headers to be sent with each request."""
+        return self._client.headers
+
+    @headers.setter
+    def headers(self, headers) -> None:
+        self._client.headers = headers
 
     async def _send_and_get_json(self, request_json: str) -> Union[bytes, str]:
-        return (await self.client.post(self.url, content=request_json)).content
+        return (await self._client.post(self.url, content=request_json)).content
 
 
 class RPCHTTPClient(RPCClient):
     """A JSON-RPC HTTP Client."""
 
     def __init__(self, url: str, headers: Optional[dict] = None) -> None:
-        self.client = httpx.Client()
+        self._client = httpx.Client()
         headers = headers or {}
         headers["Content-Type"] = "application/json"
-        self.client.headers = headers
+        self._client.headers = headers
         self.url = url
         super(RPCHTTPClient, self).__init__()
 
     def __del__(self) -> None:
-        self.client.close()
+        self._client.close()
+
+    @property
+    def headers(self) -> Headers:
+        """HTTP headers to be sent with each request."""
+        return self._client.headers
+
+    @headers.setter
+    def headers(self, headers) -> None:
+        self._client.headers = headers
 
     def _send_and_get_json(self, request_json: str) -> Union[bytes, str]:
-        return self.client.post(self.url, content=request_json).content
+        return self._client.post(self.url, content=request_json).content
