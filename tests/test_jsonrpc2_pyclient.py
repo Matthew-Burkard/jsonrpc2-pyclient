af1cf1690fc216b36e333be8d2b8f661684b7ac4
Matthew Burkard <matthewjburkard@gmail.com>
2021-12-21 20:29:06 -0500

    test: add unit tests for RPCHTTPClient

--- tests/test_jsonrpc2_pyclient.py
+++ tests/test_jsonrpc2_pyclient.py
@@ -3,7 +3,10 @@ import asyncio
 import unittest
 from dataclasses import dataclass
 
-from jsonrpc2pyclient.httpclient import AsyncRPCHTTPClient
+from jsonrpc2pyclient.httpclient import AsyncRPCHTTPClient, RPCHTTPClient
+
+# FIXME These tests need to stand alone.
+#  Use some sort of mock server.
 
 
 @dataclass
@@ -27,12 +30,22 @@ class AsyncMathClient(AsyncRPCHTTPClient):
         return Vector3(**await self.call("get_distance", [target, other]))
 
 
-class RPCClientTest(unittest.TestCase):
+class MathClient(RPCHTTPClient):
+    """Test implementation of RPCHTTPClient."""
+
+    def add(self, a: float, b: float) -> float:
+        """Basic test function."""
+        return self.call("add", [a, b])
+
+    def get_distance(self, target: Vector3, other: Vector3) -> Vector3:
+        """Test function with models."""
+        return Vector3(**self.call("get_distance", [target, other]))
+
+
+class AsyncRPCClientTest(unittest.TestCase):
     def __init__(self, *args) -> None:
-        # FIXME These tests need to stand alone.
-        #  Use some sort of mock server.
         self.client = AsyncMathClient("http://localhost:9080/api/v1/")
-        super(RPCClientTest, self).__init__(*args)
+        super(AsyncRPCClientTest, self).__init__(*args)
 
     def test_basic_func(self) -> None:
         self.assertEqual(4, asyncio.run(self.client.add(2, 2)))
@@ -41,4 +54,19 @@ class RPCClientTest(unittest.TestCase):
         self.assertEqual(
             Vector3(1, 1, 1),
             asyncio.run(self.client.get_distance(Vector3(2, 2, 2), Vector3(1, 1, 1))),
+        )
+
+
+class RPCClientTest(unittest.TestCase):
+    def __init__(self, *args) -> None:
+        self.client = MathClient("http://localhost:9080/api/v1/")
+        super(RPCClientTest, self).__init__(*args)
+
+    def test_basic_func(self) -> None:
+        self.assertEqual(4, self.client.add(2, 2))
+
+    def test_model_func(self) -> None:
+        self.assertEqual(
+            Vector3(1, 1, 1),
+            self.client.get_distance(Vector3(2, 2, 2), Vector3(1, 1, 1)),
         )
