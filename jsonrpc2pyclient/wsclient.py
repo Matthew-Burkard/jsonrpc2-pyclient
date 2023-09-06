52ecb6a4f13cbbdd6e22796004b81b18a62ffc99
Matthew Burkard <matthewjburkard@gmail.com>
2023-09-06 15:43:06 -0400

    feat!: re-write websocket client
    New one does not require `async with` and can make multiple async requests at once.

--- jsonrpc2pyclient/wsclient.py
+++ jsonrpc2pyclient/wsclient.py
@@ -1,7 +1,8 @@
 """RPCClient Websocket implementation."""
 __all__ = ("AsyncRPCWSClient",)
 
-from types import TracebackType
+import asyncio
+import json
 from typing import Optional, Union
 
 import websockets
@@ -15,41 +16,43 @@ class AsyncRPCWSClient(AsyncRPCClient):
 
     def __init__(self, url: str, headers: Optional[dict[str, str]] = None) -> None:
         """Init async WS client with URL and headers."""
-        self._headers = headers
+        self.headers = headers
         self.url = url
         self.websocket: Optional[WebSocketClientProtocol] = None
+        self._message_resolvers: dict[str | int, asyncio.Event] = {}
+        self._responses: dict[str | int, str] = {}
         super(AsyncRPCWSClient, self).__init__()
 
-    async def __aenter__(self) -> "AsyncRPCWSClient":
-        """Open WebSocket connection."""
-        # Type ignore because `websockets` typing is broken.
-        self.websocket = await websockets.connect(  # type: ignore
-            self.url, extra_headers=self.headers
-        )
-        return self
+    async def connect(self):
+        """Connect to WebSocket server."""
+        self.websocket = await websockets.connect(self.url, extra_headers=self.headers)
+        asyncio.create_task(self._receive_messages())
 
-    async def __aexit__(
-        self,
-        exc_type: Optional[type[BaseException]],
-        exc_value: Optional[BaseException],
-        tb: Optional[TracebackType],
-    ) -> None:
-        """Close WebSocket connection."""
-        # Type ignore because `websockets` typing is broken.
-        await self.websocket.close()  # type: ignore
-
-    @property
-    def headers(self) -> Optional[dict[str, str]]:
-        """Headers used on websocket connect."""
-        return self._headers
-
-    @headers.setter
-    def headers(self, headers: dict[str, str]) -> None:
-        self._headers = headers
+    async def _receive_messages(self):
+        while True:
+            try:
+                message = await self.websocket.recv()
+                response = json.loads(message)
+                request_id = response.get("id")
+                self._responses[request_id] = message
+                resolver = self._message_resolvers.pop(request_id)
+                resolver.set()
+            except websockets.ConnectionClosedOK:
+                break
 
     async def _send_and_get_json(self, request_json: str) -> Union[bytes, str]:
-        if self.websocket is None:
-            msg = "WebSocket connection is not established."
-            raise ValueError(msg)
-        await self.websocket.send(request_json)
-        return await self.websocket.recv()
+        if self.websocket and self.websocket.open:
+            await self.websocket.send(request_json)
+        else:
+            msg = "WebSocket is not open, call `connect()` first."
+            raise Exception(msg)
+        event = asyncio.Event()
+        request_id = json.loads(request_json)["id"]
+        self._message_resolvers[request_id] = event
+        await event.wait()
+        return self._responses.pop(request_id)
+
+    async def close(self):
+        """Close connection to WebSocket server."""
+        if self.websocket and self.websocket.open:
+            await self.websocket.close()
